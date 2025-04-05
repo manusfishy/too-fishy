952916a77f0e99af8f3e3e273c59b651c69f5666
Sheak Sadi <sheaksadi@gmail.com>
2025-04-06 05:49:12 +0600

    inventory looks better and works better

--- scripts/inventory/inv_ui.gd
+++ scripts/inventory/inv_ui.gd
@@ -1,23 +1,60 @@
-extends Control
+extends PanelContainer
 
 var is_open = false
 
+enum InventoryValues {FishesCaught, TotalWeight, TotalValue}
+var inventory = {
+	InventoryValues.FishesCaught: 0,
+	InventoryValues.TotalWeight: 0,
+	InventoryValues.TotalValue: 0,
+}
+
+@onready var grid = $VBoxContainer/GridContainer
+
 func _ready():
+	update_display()
 	close()
-	
-func _process(delta: float) -> void:
+
+func _process(delta):
+	if GameState.isDocked:
+		close()
 	if Input.is_action_just_pressed("inv_toggle"):
+		if GameState.isDocked:
+			return
 		if is_open:
 			close()
-		else :
+		else:
 			open()
+	if is_open:
+		update_display()
 
-	
-func close():
-	visible = false
-	is_open = false
-	
 func open():
 	visible = true
 	is_open = true
+	update_display()
+
+func close():
+	visible = false
+	is_open = false
+
+func update_inventory_value(key, value):
+	inventory[key] = value
+	update_display()
+
+func update_display():
+	# Clear existing children (except any permanent ones you might have)
+	for child in grid.get_children():
+		child.queue_free()
 	
+	# Create text elements
+	var fishes_text = Label.new()
+	fishes_text.text = "Fishes: %d" % inventory[InventoryValues.FishesCaught]
+	grid.add_child(fishes_text)
+	
+	var weight_text = Label.new()
+	weight_text.text = "Weight: %.1f kg" % inventory[InventoryValues.TotalWeight]
+	grid.add_child(weight_text)
+	
+	var value_text = Label.new()
+	value_text.text = "Value: $%.2f" % inventory[InventoryValues.TotalValue]
+	grid.add_child(value_text)
