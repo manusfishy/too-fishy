12de6d098a15fe9d1da9e7b337af413f38b45f12
Jo Bettaque <bettaque@gmail.com>
2025-04-07 01:58:04 +0200

    particles and light changes

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,18 +1,34 @@
 extends Camera3D
 
+@export var directional_light: DirectionalLight3D
+
 var environment_color_map = {
-	GameState.Stage.SURFACE: Color.AQUAMARINE * 0.4,
-	GameState.Stage.DEEP: Color.AQUA,
-	GameState.Stage.DEEPER: Color.CADET_BLUE,
-	GameState.Stage.SUPERDEEP: Color.DARK_CYAN,
-	GameState.Stage.HOT: Color.CORNFLOWER_BLUE,
-	GameState.Stage.LAVA: Color.ORANGE_RED,
-	GameState.Stage.VOID: Color.BLACK
+	GameState.Stage.SURFACE: Color.from_rgba8(30, 110, 163, 255),
+	GameState.Stage.DEEP: Color.from_rgba8(30, 110, 163, 255),
+	GameState.Stage.DEEPER: Color.from_rgba8(12, 59, 94, 255),
+	GameState.Stage.SUPERDEEP: Color.from_rgba8(12, 59, 94, 255),
+	GameState.Stage.HOT: Color.from_rgba8(217, 103, 4, 255),
+	GameState.Stage.LAVA: Color.from_rgba8(217, 46, 4, 255),
+	GameState.Stage.VOID: Color.from_rgba8(0, 0, 0, 255)
+}
+
+var environment_light_map = {
+	GameState.Stage.SURFACE: 1.0,
+	GameState.Stage.DEEP: 0.8,
+	GameState.Stage.DEEPER: 0.5,
+	GameState.Stage.SUPERDEEP: 0.1,
+	GameState.Stage.HOT:  0.2,
+	GameState.Stage.LAVA: 1.0,
+	GameState.Stage.VOID: 0.1,
 }
 
 func _ready():
 	environment.fog_light_color = environment_color_map[0]
+	directional_light = get_node("/root/Node3D/DirectionalLight3D")
 
 func change_section_environment(sectionType):
 	var tween = self.create_tween()
 	tween.tween_property(environment, "fog_light_color", environment_color_map[sectionType], 1)
+	if (directional_light):
+		tween.tween_property(directional_light, "light_energy", environment_light_map[sectionType], 3)
+		print(directional_light.light_energy)
