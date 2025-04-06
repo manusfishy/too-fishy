f9bbd467dca22e7d71a79b55c122cdaba872c40a
Jo Bettaque <bettaque@gmail.com>
2025-04-06 22:47:41 +0200

    implemented ui background

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,9 +1,9 @@
 extends PanelContainer
 
 func _process(delta: float) -> void:
-	$HUDContainer/DepthLabel.text = "Depth: %sm" % GameState.depth
-	$HUDContainer/MaxDepthLabel.text = "Depth Reached: %sm" % GameState.maxDepthReached
-	$HUDContainer/MoneyLabel.text = "Money: %s" % GameState.money
-	$HUDContainer/CurrentStageLabel.text = "%s" % Strings.stageNames[GameState.playerInStage]
-	$HUDContainer/HealthBar.value = GameState.health
-	$HUDContainer/PressureHeadroomBar.value = GameState.headroom / (GameState.upgrades[GameState.Upgrade.DEPTH_RESISTANCE] + 1)
+	$MarginContainer/HUDContainer/DepthLabel.text = "Depth: %sm" % GameState.depth
+	$MarginContainer/HUDContainer/MaxDepthLabel.text = "Depth Reached: %sm" % GameState.maxDepthReached
+	$MarginContainer/HUDContainer/MoneyLabel.text = "Money: %s" % GameState.money
+	$MarginContainer/HUDContainer/CurrentStageLabel.text = "%s" % Strings.stageNames[GameState.playerInStage]
+	$MarginContainer/HUDContainer/HealthBar.value = GameState.health
+	$MarginContainer/HUDContainer/PressureHeadroomBar.value = GameState.headroom / (GameState.upgrades[GameState.Upgrade.DEPTH_RESISTANCE] + 1)
