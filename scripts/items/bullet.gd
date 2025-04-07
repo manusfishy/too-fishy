e0248309ebe36b7b133254175f28bc4ae47f5341
Johannes Wanner <johannes.wanner@web.com>
2025-04-07 17:24:54 +0200

    ak working now

--- scripts/items/bullet.gd
+++ scripts/items/bullet.gd
@@ -1,4 +1,4 @@
-extends Area3D
+extends Node3D
 
 var speed = 50.0 # Meters per second
 var velocity = Vector3.ZERO
@@ -9,32 +9,22 @@ var submarine = null # Reference to submarine that shot this bullet
 func _ready():
 	submarine = GameState.player_node
 	# Connect collision signal
-	connect("body_entered", _on_body_entered)
+	# connect("body_entered", _on_body_entered)
 	# Start a timer to despawn
 	await get_tree().create_timer(lifetime).timeout
 	queue_free()
 
 func _physics_process(delta):
-	# Move bullet in its forward direction
-	# print("bullet process!")
+	# Move harpoon# Move in the direction set by the submarine
 	translate(direction * speed * delta)
 	
 	# Check for collisions with fish
-	#for body in $Area3D.get_overlapping_bodies():
-	#	if body.is_in_group("fishes"):
-	#		if submarine:
-	#			submarine.catch_fish(body) # Call catch_fish on submarine
-	#		queue_free()
-	#		break # Only catch one fish per harpoon
-	#	if body.is_in_group("boss"):
-	#		Boss.attackBoss() # Remove bullet on impact
-
-func _on_body_entered(body):
-	# Handle collision 
-	if body.is_in_group("fishes"):
-		if submarine:
-			submarine.catch_fish(body) # Call catch_fish on submarine
-		
-	if body.is_in_group("boss"):
-			Boss.attackBoss() # Remove bullet on impact
-	queue_free()
+	for body in $Area3D.get_overlapping_bodies():
+		if body.is_in_group("fishes"):
+			if submarine:
+				submarine.catch_fish(body) # Call catch_fish on submarine
+			#body.queue_free() # Remove fish *handled in fish.gd
+			queue_free() # Remove harpoon after hit
+			break # Only catch one fish per harpoon
+		if body.is_in_group("boss"):
+			Boss.attackBoss()
